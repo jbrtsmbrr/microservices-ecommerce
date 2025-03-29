13f3ab02da06198a8dc2cf1794e95f81e0a92fbf
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-03-29 20:39:04 +0800

    init: express product routes

---
+++ products/src/frameworks/express/routes/product-routes.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { all_products_controller } from "../../../adapters/controllers";
+
+const product_router = Router();
+
+// Create
+product_router.post('/', async function (_, res) {
+    res.send('Product Created!')
+});
+
+// Update
+product_router.put('/:id', async function (_, res) {
+    res.send('Product Updated!');
+});
+
+// Find all
+product_router.get('/', async function (req, res) {
+    await all_products_controller.handle(req, res);
+});
+
+// Find One
+product_router.get('/:id', async function (_, res) {
+    res.send('Product Selected!');
+});
+
+
+// Delete
+product_router.delete('/:id', async function (_, res) {
+    res.send('Product Deleted!');
+});
+
+export default product_router;
