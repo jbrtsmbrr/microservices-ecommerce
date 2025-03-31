0166ea24515d02007ed9b328e8d321edb865d0eb
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-03-31 19:14:42 +0800

    init: EventPublisher class

---
+++ orders/src/adapters/services/events/EventPublisher.ts
@@ -0,0 +1,18 @@
+import { IPublisher } from "../../../application/interfaces/services/IPublisher";
+import RabbitMQ from "../../../frameworks/rabbitmq/connection";
+
+
+export class EventPublisher implements IPublisher {
+    public async publish(topic: string, payload: any): Promise<void> {
+        const connection = await RabbitMQ.getConnection();
+        const channel = await connection.createChannel();
+
+        channel.assertExchange('ORDER_FAILED_EXCHANGE', 'fanout', { durable: false });
+        channel.publish('ORDER_FAILED_EXCHANGE', '', Buffer.from(JSON.stringify(payload)));
+
+        // channel.assertQueue(topic, { durable: false });
+        // channel.sendToQueue(topic, Buffer.from(JSON.stringify(payload)));
+
+        console.log(`[x] Order Service Published an event called: ${topic}`)
+    }
+}
