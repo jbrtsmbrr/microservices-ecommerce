e37f53088adf15c27156bc749c3c97458c1272f1
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-03-31 19:17:02 +0800

    init: order service index

---
+++ orders/src/frameworks/express/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import orders_router from "./routes/orders-route";
+import { StartConsumers } from "../../adapters/services/events";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/orders", orders_router);
+
+export default function startOrderService() {
+    app.listen(process.env.PORT, () => {
+        console.log(`[/] Order Service Running at http://localhost:${process.env.PORT}`);
+    })
+}
