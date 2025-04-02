6766e4631309f5283d823b53eb7b4ef51591f17b
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-04-02 19:29:07 +0800

    refactor: added create product controller in controller index

--- products/src/adapters/controllers/index.ts
+++ products/src/adapters/controllers/index.ts
@@ -1,5 +1,7 @@
+import { CreateProductUseCase } from "../../application/usecases/create-product.usecase";
 import { GetProductsUseCase } from "../../application/usecases/get-all-products.usecase";
 import { ProductRepository } from "../persistence/product-repository";
+import { CreateProductController } from "./create-product.controller";
 import { GetProductsController } from "./get-all-products.controller";
 
 // Repositories
@@ -7,6 +9,8 @@ const products_repository = new ProductRepository();
 
 // Use cases
 const get_all_usecase = new GetProductsUseCase(products_repository);
+const create_product_usecase = new CreateProductUseCase(products_repository);
 
 // Controllers
 export const all_products_controller = new GetProductsController(get_all_usecase);
+export const create_product_controller = new CreateProductController(create_product_usecase);
