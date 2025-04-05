90369d04553b1896052d91d8ccecc0e27aa6b8e2
jbrtsmbrr <jbrt.dev@gmail.com>
2025-04-05 13:12:51 +0800

    refactor: added OnlyRequired utility interface

--- orders/src/application/use-cases/types/utils.ts
+++ orders/src/application/use-cases/types/utils.ts
@@ -1,1 +1,2 @@
-export type PartialField<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+export type PartialField<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+export type OnlyRequired<T, K extends keyof T> = Partial<Omit<T, K>> & Pick<T, K>;
