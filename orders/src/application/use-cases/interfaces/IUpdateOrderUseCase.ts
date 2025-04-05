b7533922f97e8ed3574ba07a9718ec3b6b7f7808
jbrtsmbrr <jbrt.dev@gmail.com>
2025-04-05 13:12:24 +0800

    init: IUpdateOrderUseCase interface

--- orders/src/application/use-cases/interfaces/IUpdateOrderUseCase.ts
+++ orders/src/application/use-cases/interfaces/IUpdateOrderUseCase.ts
@@ -1,5 +1,7 @@
-import { OrderStatus } from "./common";
+import { OnlyRequired } from "../types/utils";
+import { OrderObject } from "./common";
 
+export type UpdateOrderParams = OnlyRequired<Omit<OrderObject, 'payment_method'>, 'id'>;
 export interface IUpdateOrderUseCase {
-    execute(order_id: string, status: OrderStatus): Promise<{ updated_entries: number}>;
+    execute(order: UpdateOrderParams): Promise<{ updated_entries: number }>;
 }
