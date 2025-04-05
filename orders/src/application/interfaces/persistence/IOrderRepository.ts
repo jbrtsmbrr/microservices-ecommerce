b4d452bf1f774ec330a472be7f7c73e839d22c6c
jbrtsmbrr <jbrt.dev@gmail.com>
2025-04-05 13:14:39 +0800

    refactor: updated findOne and updateOne in IOrderRepository interface

--- orders/src/application/interfaces/persistence/IOrderRepository.ts
+++ orders/src/application/interfaces/persistence/IOrderRepository.ts
@@ -1,5 +1,8 @@
 import { OrderDetails, OrderObject } from "../../use-cases/interfaces/common";
+import { UpdateOrderParams } from "../../use-cases/interfaces/IUpdateOrderUseCase";
 
 export interface IOrderRepository {
     save(order: OrderDetails): Promise<OrderObject | null>;
+    findOne(order: Partial<Omit<OrderObject, 'payment_method'>>): Promise<OrderObject | null>;
+    updateOne(order: UpdateOrderParams): Promise<{ affected_entries: number }>;
 }
