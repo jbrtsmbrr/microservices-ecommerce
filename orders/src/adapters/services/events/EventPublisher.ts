42a5c717882307395dc501b5bb031bae132c416c
jbrtsmbrr <jbrt.dev@gmail.com>
2025-04-05 13:11:29 +0800

    refactor: apply changes in RabbitMQ adapter

--- orders/src/adapters/services/events/EventPublisher.ts
+++ orders/src/adapters/services/events/EventPublisher.ts
@@ -4,15 +4,14 @@ import RabbitMQ from "../../../frameworks/rabbitmq/connection";
 
 export class EventPublisher implements IPublisher {
     public async publish(topic: string, payload: any): Promise<void> {
-        const connection = await RabbitMQ.getConnection();
-        const channel = await connection.createChannel();
+        const channel = await RabbitMQ.createChannel();
 
-        channel.assertExchange('ORDER_FAILED_EXCHANGE', 'fanout', { durable: false });
-        channel.publish('ORDER_FAILED_EXCHANGE', '', Buffer.from(JSON.stringify(payload)));
+        // channel.assertExchange('ORDER_FAILED_EXCHANGE', 'fanout', { durable: false });
+        // channel.publish('ORDER_FAILED_EXCHANGE', '', Buffer.from(JSON.stringify(payload)));
 
-        // channel.assertQueue(topic, { durable: false });
-        // channel.sendToQueue(topic, Buffer.from(JSON.stringify(payload)));
+        channel.assertQueue(topic, { durable: false });
+        channel.sendToQueue(topic, Buffer.from(JSON.stringify(payload)));
 
-        console.log(`[x] Order Service Published an event called: ${topic}`)
+        console.log(`[x] Order Service Published an event called: ${topic}`);
     }
 }
