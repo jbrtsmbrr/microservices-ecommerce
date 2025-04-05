f1e96db302918a149173848e3567f6311a1d968f
jbrtsmbrr <jbrt.dev@gmail.com>
2025-04-05 13:16:12 +0800

    init: Order consumers

--- orders/src/adapters/services/events/index.ts
+++ orders/src/adapters/services/events/index.ts
@@ -1,7 +1,10 @@
 import { UpdateOrderUseCase } from "../../../application/use-cases/update-order.usecase";
+import "./consumers/order-successs-payment.consumer";
 import OrderFailedConsumer from "./consumers/order-failed.consumer";
+import { OrderRepository } from "../../persistence/OrderRepository";
 
-const update_order_usecase = new UpdateOrderUseCase();
+const order_repository = new OrderRepository();
+const update_order_usecase = new UpdateOrderUseCase(order_repository);
 
 const order_failed_consumer = new OrderFailedConsumer(update_order_usecase);
 
