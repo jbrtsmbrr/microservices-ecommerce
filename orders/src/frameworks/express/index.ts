10e13fdbe490965f79ba3a5d21f4e37c02ef00e2
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-04-07 23:30:37 +0800

    refactor: added trackings router in express routes

--- orders/src/frameworks/express/index.ts
+++ orders/src/frameworks/express/index.ts
@@ -2,8 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import orders_router from "./routes/orders-route";
-import http from "http";
-import { Server } from "socket.io";
+import trackingRouter from "./routes/trackings-route";
 
 dotenv.config();
 
@@ -11,37 +10,12 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors({
-    credentials: true,
-    origin: "*"
-}));
+app.use(cors());
 
 // Routes
 app.use("/orders", orders_router);
+app.use("/trackings", trackingRouter);
 
-const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        credentials: true,
-        origin: "*",
-        methods: ["GET"]
-    }
-});
-
-io.sockets.on("connection", (socket) => {
-    console.log("Client connected:", socket.id);
-    
-    socket.on("disconnect", () => {
-        console.log("Client disconnected:", socket.id);
-    });
-});
-
-server.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`[/] Order Service Running at http://localhost:${process.env.PORT}`);
 })
-
-// export default function startOrderService() {
-//     server.listen(process.env.PORT, () => {
-//         console.log(`[/] Order Service Running at http://localhost:${process.env.PORT}`);
-//     })
-// }
