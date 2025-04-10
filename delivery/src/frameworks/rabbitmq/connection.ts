4965e13bddb38304a40fed07650e41c80fbfd9d2
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-04-10 22:04:09 +0800

    init: RabbitMQ connection

---
+++ delivery/src/frameworks/rabbitmq/connection.ts
@@ -0,0 +1,16 @@
+import amqplib, { Channel } from "amqplib";
+
+export default class RabbitMQ {
+    private static channel: Promise<Channel> | null = null;
+
+    private constructor() { }
+
+    public static async getChannel() {
+        if (RabbitMQ.channel === null) {
+            const connection = await amqplib.connect('amqp://localhost');
+            RabbitMQ.channel = connection.createChannel();
+        }
+
+        return RabbitMQ.channel;
+    }
+}
