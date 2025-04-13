559d06690e2ef4779ebb74b345fe9f55482c0a87
jbrtsmbrr <jbrt.dev@gmail.com>
2025-04-13 16:02:46 +0800

    refactor: order ongoing delivery consumer

--- orders/src/adapters/services/events/consumers/order-delivery.consumer.ts
+++ orders/src/adapters/services/events/consumers/order-delivery.consumer.ts
@@ -5,18 +5,21 @@ import { OrderRepository } from "../../../persistence/OrderRepository";
 const order_repository = new OrderRepository();
 const update_order = new UpdateOrderStatusUseCase(order_repository);
 
-(async function() {
+(async function () {
     const channel = await RabbitMQ.createChannel();
-    channel.assertQueue('DELIVERY_PREPARED_ORDER_QUEUE');
-    channel.bindQueue('DELIVERY_PREPARED_ORDER_QUEUE', 'SCHEDULED_DELIVERY_EXCHANGE', '');
+    channel.assertExchange('ONGOING_DELIVERY_EXCHANGE', 'fanout')
+    channel.assertQueue('ON_GOING_DELIVERY_QUEUE');
+    channel.bindQueue('ON_GOING_DELIVERY_QUEUE', 'ONGOING_DELIVERY_EXCHANGE', '');
 
-    channel.consume('DELIVERY_PREPARED_ORDER_QUEUE', async (msg) => {
+    channel.consume('ON_GOING_DELIVERY_QUEUE', async (msg) => {
         if (!msg) return;
 
         // @ts-ignore
-        const { order_id } = JSON.parse(msg.content as string) as { order_id: string };
+        const { orders } = JSON.parse(msg.content as string) as { orders: string[] };
 
-        update_order.execute(order_id, 'ON_GOING_DELIVERY');
+        for (let order of orders) {
+            await update_order.execute(order, 'ON_GOING_DELIVERY');
+        }
 
         channel.ack(msg);
     })
