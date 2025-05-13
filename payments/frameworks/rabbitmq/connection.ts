3f6ed1dac45e791ed7018805835acd0c4d02ad60
jbrtsmbrr <jbrt.dev@gmail.com>
2025-05-13 13:31:31 +0800

    refactor: use rabbitmq url from env

--- payments/frameworks/rabbitmq/connection.ts
+++ payments/frameworks/rabbitmq/connection.ts
@@ -8,7 +8,8 @@ export default class RabbitMQ {
 
   public static async createChannel() {
     if (RabbitMQ.channel === null) {
-      const connection = await amqplib.connect('amqp://localhost');
+      const url = process.env.RABBITMQ_URL ?? 'amqp://localhost';
+      const connection = await amqplib.connect(url);
       RabbitMQ.channel = connection.createChannel();
     }
 
